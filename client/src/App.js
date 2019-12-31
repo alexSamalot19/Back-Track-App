a5275a827199bdf3482c580e13d2b01f789c6e58
alexSam <alexsamalot19@gmail.com>
2019-12-31 10:26:41 -0500

    basic design concept before routing

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Button from "./components/Button";
 import API from "./utils/API";
 import moment from "moment";
 import { RecipeList, RecipeListItem } from "./components/RecipeList";
+// import { EventList, EventListItem } from "./components/Calendar";
 import { Container, Row, Col } from "./components/Grid";
 import { GOOGLE_API_KEY, CALENDAR_ID } from "./config.js";
 // import { gapi } from "gapi-script";
@@ -14,7 +15,12 @@ import { GOOGLE_API_KEY, CALENDAR_ID } from "./config.js";
 class App extends Component {
   state = {
     recipes: [],
-    recipeSearch: ""
+    recipeSearch: "",
+    time: moment().format("dd, Do MMMM, h:mm A"),
+    events: [],
+    isBusy: false,
+    isEmpty: false,
+    isLoading: true
   };
 
   handleInputChange = event => {
@@ -31,12 +37,10 @@ class App extends Component {
   };
 
   handleCalendarSubmit = event => {
-    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
-    // event.preventDefault();
-    // API.getCalendarEvents(this.state.calendarSearch)
-    //   .then(res => this.setState({ calendar: res.events }))
-    //   .catch(err => console.log(err));
+    this.getEvents();
+  };
 
+  handleMakeTopic = event => {
     this.getEvents();
   };
 
@@ -49,14 +53,53 @@ class App extends Component {
   };
 
   render() {
+    const { time, events } = this.state;
+
+    let eventsList = events.map(function(event) {
+      return (
+        <a
+          className="list-group-item"
+          href={event.htmlLink}
+          target="_blank"
+          key={event.id}
+        >
+          {event.summary}{" "}
+          <span className="badge">
+            {moment(event.start.dateTime).format("h:mm a")},{" "}
+            {moment(event.end.dateTime).diff(
+              moment(event.start.dateTime),
+              "minutes"
+            )}{" "}
+            minutes, {moment(event.start.dateTime).format("MMMM Do")}{" "}
+          </span>
+        </a>
+      );
+    });
+
     return (
       <div>
         <Nav />
         <Jumbotron />
+        <Button
+          onClick={this.handleCalendarSubmit}
+          type="success"
+          className="input-lg"
+        >
+          Calendar
+        </Button>
+
+        <Button
+          onClick={this.handleMakeTopic}
+          type="success"
+          className="input-lg"
+        >
+          Make Topic
+        </Button>
+
         <Container>
           <Row>
             <Col size="md-12">
-              <form>
+              {/* <form>
                 <Container>
                   <Row>
                     <Col size="xs-9 sm-10">
@@ -75,22 +118,24 @@ class App extends Component {
                       >
                         Search
                       </Button>
-                      <Button
-                        onClick={this.handleCalendarSubmit}
-                        type="success"
-                        className="input-lg"
-                      >
-                        Calendar
-                      </Button>
                     </Col>
                   </Row>
                 </Container>
-              </form>
+              </form> */}
             </Col>
           </Row>
           <Row>
             <Col size="xs-12">
-              {!this.state.recipes.length ? (
+              <div className="upcoming-meetings">
+                <div className="current-time">{time}, 2018</div>
+                <h1>Upcoming Meetings</h1>
+                <div className="list-group">
+                  {/* {this.state.isLoading && loadingState} */}
+                  {events.length > 0 && eventsList}
+                  {/* {this.state.isEmpty && emptyState} */}
+                </div>
+              </div>
+              {/* {!this.state.recipes.length ? (
                 <h1 className="text-center">No Recipes to Display</h1>
               ) : (
                 <RecipeList>
@@ -106,7 +151,7 @@ class App extends Component {
                     );
                   })}
                 </RecipeList>
-              )}
+              )} */}
             </Col>
           </Row>
         </Container>
@@ -135,15 +180,38 @@ class App extends Component {
             let events = response.result.items;
             console.log(events);
             console.log(response);
+            let sortedEvents = events.sort(function(a, b) {
+              return (
+                moment(b.start.dateTime).format("YYYYMMDD") -
+                moment(a.start.dateTime).format("YYYYMMDD")
+              );
+            });
+            if (events.length > 0) {
+              that.setState(
+                {
+                  events: sortedEvents,
+                  isLoading: false,
+                  isEmpty: false
+                }
+                // ,
+                // () => {
+                //   that.setStatus();
+                // }
+              );
+            } else {
+              that.setState({
+                isBusy: false,
+                isEmpty: true,
+                isLoading: false
+              });
+            }
           },
           function(reason) {
             console.log(reason);
           }
         );
     }
-
-    console.log(gapi);
-    window.gapi.load("client", start);
+    gapi.load("client", start);
   }
 }
 
--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,9 +4,13 @@ import "./style.css";
 function Jumbotron() {
   return (
     <div className="jumbotron text-center">
-      <h1>React Recipes</h1>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.recipepuppy.com/about/api/">
-        Powered by Recipe Puppy
+      <h1>Back Track</h1>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://www.recipepuppy.com/about/api/"
+      >
+        Group Challenges Taken to the Next Level
       </a>
     </div>
   );
